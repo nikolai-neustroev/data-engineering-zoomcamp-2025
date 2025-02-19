fa38a9066a62c7dbc499a4729890e642a2d88930
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2025-02-19 21:14:03 +0100

    fix

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -1,46 +1,53 @@
 WITH base AS (
     SELECT
+        service_type,
         pickup_datetime,
         total_amount,
         EXTRACT(YEAR FROM pickup_datetime) AS year,
-        EXTRACT(QUARTER FROM pickup_datetime) AS quarter,
-        EXTRACT(MONTH FROM pickup_datetime) AS month
+        EXTRACT(QUARTER FROM pickup_datetime) AS quarter
     FROM {{ ref('fact_trips') }}
 ),
 
 quarterly AS (
     SELECT
+        service_type,
         year,
         quarter,
         SUM(total_amount) AS quarterly_revenue
-    FROM base
-    GROUP BY year, quarter
+    FROM 
+        base
+    GROUP BY 
+        service_type, 
+        year, 
+        quarter
 ),
 
 quarterly_yoy AS (
     SELECT
+        service_type,
         year,
         quarter,
         quarterly_revenue,
         LAG(quarterly_revenue) OVER (
-            PARTITION BY quarter 
+            PARTITION BY service_type, quarter 
             ORDER BY year
         ) AS previous_year_revenue,
         CASE 
             WHEN LAG(quarterly_revenue) OVER (
-                    PARTITION BY quarter 
+                    PARTITION BY service_type, quarter 
                     ORDER BY year
                  ) IS NULL THEN NULL
             ELSE (quarterly_revenue - LAG(quarterly_revenue) OVER (
-                      PARTITION BY quarter ORDER BY year
+                      PARTITION BY service_type, quarter ORDER BY year
                   )) / LAG(quarterly_revenue) OVER (
-                      PARTITION BY quarter ORDER BY year
+                      PARTITION BY service_type, quarter ORDER BY year
                   )
         END AS yoy_growth
     FROM quarterly
 )
 
 SELECT
+    service_type,
     year,
     quarter,
     quarterly_revenue,
@@ -48,4 +55,7 @@ SELECT
     yoy_growth,
     CONCAT(year, '/Q', quarter) AS year_quarter
 FROM quarterly_yoy
-ORDER BY year, quarter
+ORDER BY 
+    service_type,
+    year, 
+    quarter
